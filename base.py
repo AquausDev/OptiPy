03ca2c1744594dbf6cac6cd63e74b3383a33ebaf
Michael <aquausdev@gmail.com>
2016-09-30 00:43:54 +0100

    Laid down base code
    Need to fix issue where it replaces into two lines

---
+++ base.py
@@ -0,0 +1,44 @@
+#OptiPy - a python code optimisation program made in python
+import fileinput
+
+class DefineFixes:
+    """outlies all the functions that run the fixes"""
+    def switch_single_quotes():
+        """switches quotes from ' to " """
+        #takes the users file name
+        file_name = input("Enter the python file name: ")
+
+        #opens the file, backs it up and then replaces
+        with fileinput.FileInput(file_name+".py", inplace=True, backup='.bak') as file:
+            #checks each line for ' then replaces it with "
+            for line in file:
+                print(line.replace("('", '("'), end='')
+                print(line.replace("')", '")'), end='')
+                
+    def switch_double_quotes():
+        """switches quotes from " to ' """
+        #takes the users file name
+        file_name = input("Enter the python file name: ")
+
+        #opens the file, backs it up and then replaces
+        with fileinput.FileInput(file_name+".py", inplace=True, backup='.bak') as file:
+            #checks each line for ' then replaces it with "
+            for line in file:
+                print(line.replace('("', "('"), end='')
+                print(line.replace('")', "')"), end='')
+
+class RunFixes:
+    """runs all the fixing functions"""      
+    def manage_switches():
+        """runs the main section of switches of sections"""
+        #runs the single quote switch
+        ask1 = input("Switch single quotes? ")
+        if ask1 == "yes":
+            DefineFixes.switch_single_quotes()
+            
+        #runs the double quote switch
+        ask2 = input("Switch double quotes? ")
+        if ask2 == "yes":
+            DefineFixes.switch_double_quotes()
+            
+RunFixes.manage_switches()
